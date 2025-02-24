1fdff9a36ad024de87403eaf63b7ccaa34f2b3fd
HSharma <70624640+hsharma700@users.noreply.github.com>
2025-02-24 10:25:38 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.scss";
 import Dwn from "./Dwn";
-import ME from '../../assets/myImage.png';
+// import ME from '../../assets/myImage.png';
 import {motion} from "framer-motion"
 function Header() {
   return (
